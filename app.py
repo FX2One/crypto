d196beb1e9582d4b92ab8ed873a39bbcf0659333
FX2One <dfoxik@gmail.com>
2022-01-28 10:04:10 +0100

    UPDATE: cmc currency function, to be more reusable

--- app.py
+++ app.py
@@ -12,18 +12,19 @@ crypto = Crypto(app.config['SECRET_KEY'])
 
 @app.route("/top5")
 def cmc_currency():
-    '''access method and pass converter parameter to obtain desired currency exchange'''
-    data = crypto.get_top_5_coins('PLN')
+    '''access method and pass converter parameter as string ('USD', 'PLN', 'EUR' etc.) to obtain desired currency exchange'''
+    convert = 'PLN'
+    data = crypto.get_top_5_coins(convert)
     cryptoPrice = []
     for i in range(5):
         dict = {'name': data[i]['name'], 'slug': data[i]['slug'],
-                 'price': data[i]['quote']['PLN']['price'],
-                 'percent_change_1h': data[i]['quote']['PLN']['percent_change_1h'],
-                 'percent_change_24h': data[i]['quote']['PLN']['percent_change_24h'],
-                 'percent_change_7d': data[i]['quote']['PLN']['percent_change_7d'],
-                 'percent_change_30d': data[i]['quote']['PLN']['percent_change_30d'],
-                 'percent_change_60d': data[i]['quote']['PLN']['percent_change_60d'],
-                 'percent_change_90d': data[i]['quote']['PLN']['percent_change_90d']}
+                 'price': data[i]['quote'][convert]['price'],
+                 'percent_change_1h': data[i]['quote'][convert]['percent_change_1h'],
+                 'percent_change_24h': data[i]['quote'][convert]['percent_change_24h'],
+                 'percent_change_7d': data[i]['quote'][convert]['percent_change_7d'],
+                 'percent_change_30d': data[i]['quote'][convert]['percent_change_30d'],
+                 'percent_change_60d': data[i]['quote'][convert]['percent_change_60d'],
+                 'percent_change_90d': data[i]['quote'][convert]['percent_change_90d']}
         dict_copy = dict.copy()
         cryptoPrice.append(dict_copy)
     return render_template("index.html", price=cryptoPrice)
