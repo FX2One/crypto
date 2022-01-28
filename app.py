e801d04dcdd5e803a5fc6084a5fd349227281ed6
FX2One <dfoxik@gmail.com>
2022-01-28 09:51:46 +0100

    ADD: render view of top5 cryptos

--- app.py
+++ app.py
@@ -12,11 +12,23 @@ crypto = Crypto(app.config['SECRET_KEY'])
 
 @app.route("/top5")
 def cmc_currency():
-    '''access method and pass converter paramter to obtain desired currency exchange'''
+    '''access method and pass converter parameter to obtain desired currency exchange'''
     data = crypto.get_top_5_coins('PLN')
-    bitcoinPrice = data[0]
-    return render_template("index.html", price=bitcoinPrice)
+    cryptoPrice = []
+    for i in range(5):
+        dict = {'name': data[i]['name'], 'slug': data[i]['slug'],
+                 'price': data[i]['quote']['PLN']['price'],
+                 'percent_change_1h': data[i]['quote']['PLN']['percent_change_1h'],
+                 'percent_change_24h': data[i]['quote']['PLN']['percent_change_24h'],
+                 'percent_change_7d': data[i]['quote']['PLN']['percent_change_7d'],
+                 'percent_change_30d': data[i]['quote']['PLN']['percent_change_30d'],
+                 'percent_change_60d': data[i]['quote']['PLN']['percent_change_60d'],
+                 'percent_change_90d': data[i]['quote']['PLN']['percent_change_90d']}
+        dict_copy = dict.copy()
+        cryptoPrice.append(dict_copy)
+    return render_template("index.html", price=cryptoPrice)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
