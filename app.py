89239d36cbeef4d245705763819b9ed337173257
FX2One <dfoxik@gmail.com>
2022-01-28 23:41:34 +0100

    UPDATE: table content view ,on main page for top5

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 '''import config file from root directory'''
 app.config.from_object('config')
 
-'''pass secret key as token value taken in Crypto class'''
+'''passes secret key as token value taken in Crypto class'''
 crypto = Crypto(app.config['SECRET_KEY'])
 
 
@@ -17,20 +17,35 @@ def cmc_currency():
     data = crypto.get_top_5_coins(convert)
     cryptoPrice = []
     for i in range(5):
-        dict = {'name': data[i]['name'], 'slug': data[i]['slug'],
-                 'price': data[i]['quote'][convert]['price'],
-                 'percent_change_1h': round(data[i]['quote'][convert]['percent_change_1h'],2),
-                 'percent_change_24h': round(data[i]['quote'][convert]['percent_change_24h'],2),
-                 'percent_change_7d': round(data[i]['quote'][convert]['percent_change_7d'],2),
-                 'percent_change_30d': round(data[i]['quote'][convert]['percent_change_30d'],2),
-                 'percent_change_60d': round(data[i]['quote'][convert]['percent_change_60d'],2),
-                 'percent_change_90d': round(data[i]['quote'][convert]['percent_change_90d'],2)}
+        dict = {'name': data[i]['name'],
+                'symbol': data[i]['symbol'],
+                'price': round(data[i]['quote'][convert]['price'],2),
+                'percent_change_1h': round(data[i]['quote'][convert]['percent_change_1h'],2),
+                'percent_change_24h': round(data[i]['quote'][convert]['percent_change_24h'],2),
+                'percent_change_7d': round(data[i]['quote'][convert]['percent_change_7d'],2),
+                'percent_change_30d': round(data[i]['quote'][convert]['percent_change_30d'],2),
+                'percent_change_60d': round(data[i]['quote'][convert]['percent_change_60d'],2),
+                'percent_change_90d': round(data[i]['quote'][convert]['percent_change_90d'],2)}
         dict_copy = dict.copy()
         cryptoPrice.append(dict_copy)
-
-    return render_template("index.html", price=cryptoPrice)
+    return render_template("index.html", price=cryptoPrice, convert=convert)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+'''    for i in range(5):
+        dict = {'name': data[i]['name'], 'slug': data[i]['slug'],
+                'price': data[i]['quote'][convert]['price'],
+                'percent_change_1h': round(data[i]['quote'][convert]['percent_change_1h'],2),
+                'percent_change_24h': round(data[i]['quote'][convert]['percent_change_24h'],2),
+                'percent_change_7d': round(data[i]['quote'][convert]['percent_change_7d'],2),
+                'percent_change_30d': round(data[i]['quote'][convert]['percent_change_30d'],2),
+                'percent_change_60d': round(data[i]['quote'][convert]['percent_change_60d'],2),
+                'percent_change_90d': round(data[i]['quote'][convert]['percent_change_90d'],2)}
+        headings.append(data[i]['name'])
+        dict_copy = dict.copy()
+        cryptoPrice.append(dict_copy)
+        return render_template("index.html", price=cryptoPrice, headings=headings)
+        '''
 
 
