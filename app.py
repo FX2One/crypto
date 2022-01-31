19148e61a2c877db420aeb4f3de787aee9472afa
FX2One <dfoxik@gmail.com>
2022-01-31 09:19:17 +0100

    UPDATE: top10 view, basic display functionality added

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.config.from_object('config')
 crypto = Crypto(app.config['SECRET_KEY'])
 
 
-@app.route("/top5")
-def cmc_currency():
+@app.route("/top-5")
+def cmc_currency5():
     '''access method and pass converter parameter as string ('USD', 'PLN', 'EUR' etc.) to obtain desired currency exchange'''
     convert = 'PLN'
     data = crypto.get_top_5_coins(convert)
@@ -29,6 +29,26 @@ def cmc_currency():
         dict_copy = dict.copy()
         cryptoPrice.append(dict_copy)
     return render_template("index.html", price=cryptoPrice, convert=convert)
+
+@app.route("/top-10")
+def cmc_currency10():
+    '''access method and pass converter parameter as string ('USD', 'PLN', 'EUR' etc.) to obtain desired currency exchange'''
+    convert = 'PLN'
+    data = crypto.get_top_10_coins(convert)
+    cryptoPrice = []
+    for i in range(10):
+        dict = {'name': data[i]['name'],
+                'symbol': data[i]['symbol'],
+                'price': round(data[i]['quote'][convert]['price'],2),
+                'percent_change_1h': round(data[i]['quote'][convert]['percent_change_1h'],2),
+                'percent_change_24h': round(data[i]['quote'][convert]['percent_change_24h'],2),
+                'percent_change_7d': round(data[i]['quote'][convert]['percent_change_7d'],2),
+                'percent_change_30d': round(data[i]['quote'][convert]['percent_change_30d'],2),
+                'percent_change_60d': round(data[i]['quote'][convert]['percent_change_60d'],2),
+                'percent_change_90d': round(data[i]['quote'][convert]['percent_change_90d'],2)}
+        dict_copy = dict.copy()
+        cryptoPrice.append(dict_copy)
+    return render_template("index2.html", price=cryptoPrice, convert=convert)
 
 if __name__ == '__main__':
     app.run(debug=True)
